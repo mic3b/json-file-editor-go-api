6d349769d6ba216164626ba4212752e57dca8545
Mikolaj <mikolaj.bubacz@gmail.com>
2023-03-14 00:40:34 +0100

    Change finish controller

--- controllers/FinishController.go
+++ controllers/FinishController.go
@@ -1,16 +1,12 @@
 package controllers
 
 import (
-	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 func FinishController(c *gin.Context) {
+	c.File("temp/file.json")
 	os.Remove("temp/file.json")
-
-	c.JSON(http.StatusCreated, gin.H{
-		"message": "File has removed!",
-	})
 }
